6baa6524a3430b68e9dc583775d1daf273183a11
HenryloveGod <494225619@qq.com>
2018-10-17 17:12:53 +0800

    add nginx_lua_study

---
+++ nginx_lua_study/debug/nginx/lua/access_check.lua
@@ -0,0 +1,10 @@
+local param= require("comm.param")
+local args = ngx.req.get_uri_args()
+
+
+for k,v in ipairs(args) do ngx.log(ngx.NOTICE, k,v) end
+
+if not args.a or not args.b or not param.is_number(args.a, args.b) then
+    ngx.exit(ngx.HTTP_BAD_REQUEST)
+    return
+end
